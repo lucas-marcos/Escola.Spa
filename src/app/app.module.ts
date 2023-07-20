7a380729a6a950e03b8337977d1d6472a220b359
lucas-marcos <lucas_190201lmms@hotmail.com>
2023-07-19 22:37:38 -0300

    Salvar a escolha no banco

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,47 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
-import { EscolaComponent } from './escola/escola.component';
 import { FormsModule } from '@angular/forms';
-import { EscolaModalComponent } from './escola-modal/escola-modal.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
+// Angular Material Modules
 import { MatButtonModule } from '@angular/material/button';
-import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+// Terceiros
+import { NgxMaskModule } from 'ngx-mask';
+
+// Componentes Locais
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { EscolaComponent } from './escola/escola.component';
+import { EscolaModalComponent } from './escola-modal/escola-modal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EscolaComponent,
-    EscolaModalComponent
+    EscolaModalComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatSidenavModule,
-    MatListModule,
     FormsModule,
+    HttpClientModule,
     MatButtonModule,
-    MatInputModule,
     MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    NgxMaskModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/escola/escola.component.ts
+++ src/app/escola/escola.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-escola',
   templateUrl: './escola.component.html',
-  styleUrls: ['./escola.component.scss']
+  styleUrls: ['./escola.component.scss'],
 })
 export class EscolaComponent {
   modalAberto: boolean = false;
   escola: any = { nome: '', endereco: '', telefone: '', email: '', cnpj: '' };
   modoEdicao: boolean = false;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
   salvarEscola() {
     console.log(this.escola);
@@ -25,5 +26,29 @@ export class EscolaComponent {
     this.modalAberto = false;
     this.escola = { nome: '', endereco: '', telefone: '', email: '', cnpj: '' };
     this.modoEdicao = false;
+  }
+
+  realizarRequisicao() {
+    const url = 'https://localhost:7009/Escola';
+
+    // Define os dados a serem enviados no corpo da requisição (no formato JSON)
+    const body = {
+      nome: 'string',
+      endereco: 'string',
+      telefone: 'string',
+      email: 'user@example.com',
+      cnpj: '24.657.575/1707-80',
+    };
+debugger
+    // Realiza a requisição POST com os dados definidos
+    this.http.post(url, this.escola).subscribe(
+      (response) => {
+        console.log('Resposta da API:', response);
+        // Faça o que for necessário com os dados da resposta aqui
+      },
+      (error) => {
+        console.error('Erro na requisição:', error);
+      }
+    );
   }
 }
