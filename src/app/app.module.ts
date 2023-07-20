d8718e58a555aa9c48682a28ec82d1ac60bf379a
lucas-marcos <lucas_190201lmms@hotmail.com>
2023-07-19 21:04:22 -0300

    Adicionado angular material no modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,9 @@ import { MatListModule } from '@angular/material/list';
 import { EscolaComponent } from './escola/escola.component';
 import { FormsModule } from '@angular/forms';
 import { EscolaModalComponent } from './escola-modal/escola-modal.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -26,7 +29,10 @@ import { EscolaModalComponent } from './escola-modal/escola-modal.component';
     MatIconModule,
     MatSidenavModule,
     MatListModule,
-    FormsModule
+    FormsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
