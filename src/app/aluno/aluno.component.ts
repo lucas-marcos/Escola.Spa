cd9af32a99060bf5a2aae16024f8ce7cd86d398e
lucas-marcos <lucas_190201lmms@hotmail.com>
2023-07-20 23:36:44 -0300

    Permitir cadastrar aluno

--- src/app/aluno/aluno.component.ts
+++ src/app/aluno/aluno.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
+import { TurmaService } from '../turma.service';
 
 interface Aluno {
   id: number;
@@ -19,27 +20,56 @@ interface Aluno {
 export class AlunoComponent implements OnInit {
   modalAberto: boolean = false;
   alunos: Aluno[] = [];
-  aluno: Aluno ={
-    id: 0, cpf: '', endereco: '', idade: 0, nome: '', turmaId: 0
+  aluno: Aluno = {
+    id: 0,
+    cpf: '',
+    endereco: '',
+    idade: 0,
+    nome: '',
+    turmaId: 0,
   };
+  turmas: any[] = [];
 
   constructor(
     private http: HttpClient,
     private toastr: ToastrService,
+    private turmaService: TurmaService
   ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.listarTurmas();
+  }
 
   abrirModal() {
     this.modalAberto = true;
   }
 
-
   fecharModal() {
     this.modalAberto = false;
   }
 
-  realizarRequisicao(){
+  realizarRequisicao() {
+    let url = 'https://localhost:7009/Aluno';
 
+    if (this.aluno.id == 0) {
+      this.http.post(url, this.aluno).subscribe((result) => {
+        this.toastr.success('Aluno cadastrado com sucesso!');
+        this.fecharModal();
+      },
+      (error) => {
+        this.toastr.error('Erro ao cadastrar aluno! \n' + error.error);
+      });
+    }
+  }
+
+  listarTurmas() {
+    this.turmaService.listarTurmas().subscribe(
+      (result) => {
+        this.turmas = result;
+      },
+      (error) => {
+        this.toastr.error('Erro ao listar turmas! \n' + error.error);
+      }
+    );
   }
 }
